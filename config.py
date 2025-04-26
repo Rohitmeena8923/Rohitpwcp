c5459fb650c96d45953069a2e7b3acfc1bc5571d
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-04-27 00:02:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7890781002,6373737837]
+api_id = "27775431"
+api_hash = "b70bb1d45a1d05236671d4cc615e40f9"
+bot_token = "7920749316:AAHFVE5_BfsIPjHKTdKJHnmnmu2cAfknDxY"
+auth_users = [6414266397]
